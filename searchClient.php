337d0ddedfd8e1b65e2eeda273d83004fbb98a02
semsK <sems@live.fr>
2017-06-27 11:34:50 +0200

    repalce Text by display

---
+++ searchClient.php
@@ -0,0 +1,9 @@
+<?php // ************ code PHP objet en couches ************
+      // Script controleur page recherche Client
+
+// Module de présentation
+require("view/searchClientView.php");
+
+// Afficher page
+displaySearch();
+?>     
--- view/searchClientView.php
+++ view/searchClientView.php
@@ -1,83 +1,44 @@
- <!doctype html>
-          <html>
-              <head>
+<?php
+  require ("headView.php");
+  require ("navView.php");
+  require ("titleView.php");
+  require ("footerView.php");
 
-                  <html lang="fr">
-
-                  <meta http-equiv="Content-Type " content="text/html; charset=utf-8">
-                  <meta name="description" content="ABI-PROJECT">
-                  <meta name="viewport" content="width=device-width, initial-scale=1">
-                  <meta name="author" content="CNS-TEAM" >
-
-                  <title>Search</title>
-
-                  <link rel="stylesheet" href="css/main.css">
-                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-                  <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" charset="utf-8"></script>
+  function displaySearch(){
 
 
+?>
+<?php displayHead("Search");
+?>
+    </head>
+
+    <body>
+      <?php displayNav();?>
+      <?php displayTitle();?>
 
 
-        </head>
-<body>
-     <nav class="navbar navbar-default navbar-fixed-top">
-     <div class="container">
-       <div class="navbar-header">
-         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNav" aria-expanded="false">
-           <span class="sr-only">Toggle navigation</span>
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-           <span class="icon-bar"></span>
-         </button>
-         <a class="navbar-brand" href="index.php"></a>
-       </div>
-       <div id="myNav" class="collapse navbar-collapse">
-         <ul class="nav navbar-nav">
-           <li><a href="index.php">Home</a></li>
-                      <li><a href="#">Contact</a></li>
-        </ul>
-                 <form id="signin" class="navbar-form navbar-right" role="form" action="login.php" method="post">
-          <button type="submit" class="btn btn-primary">Login</button>
-        </form>
-              </div>
-     </div>
-   </nav>
-
-
- 
-  <div class="container">
-    <div class="jumbotron">
-      <header>
-          <div class="row">
-    <div class="col-md-3">
-      <a href="#"><img src="#" border="0" class="img-responsive"/></a>
-    </div>
-    <div class="col-md-9">
-      <div class="row">
-        <h1>ABI</h1>
+         <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+          <ul class="nav navbar-nav">
+            <li>Accueil</li>
+            <li>Liens</li>
+            <li>Témoignages</li>
+            <li><a href="#">Références</a> </li>
+          </ul>
+          <form class="navbar-form navbar-right inline-form">
+            <div class="form-group">
+              <input type="search" class="input-sm form-control" placeholder="Recherche">
+              <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+            </div>
+          </form>
         </div>
-        <div class="row">
-          <div class="lead">
-            Commercial Management
+      </nav>
+
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-  </div>
+        </body>
 
-        </header>
-    </div>
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 text-center">
-        <h1>Bienvenue !</h1>
-              </div>
-    </div>
-  </div>
-
-</body>
-
-  </html>
+<?php
+displayFooter();
+}
+?>
