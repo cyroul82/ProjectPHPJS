8d13582c2435f44fae104c60ebcfe01153502664
nguignard <nguignard@gmail.com>
2017-06-27 11:03:14 +0200

    new file

--- clientListController.php
+++ clientListController.php
@@ -3,6 +3,10 @@
 // This controller update a list of all the Clients
 
 require("view/clientListView.php")
+require("dao/cnsDao.php")
+$data = array();
+$data = cnsDao::AllClientList();
+
 $erreur="";
 displayClientList($erreur);
 
