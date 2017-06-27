d24c50c169fab1b916ddf49e2f46087c9646b93d
semsK <sems@live.fr>
2017-06-27 11:06:28 +0200

    newfile SearchClientView

---
+++ view/searchClientView.php
@@ -0,0 +1,83 @@
+ <!doctype html>
+          <html>
+              <head>
+
+                  <html lang="fr">
+
+                  <meta http-equiv="Content-Type " content="text/html; charset=utf-8">
+                  <meta name="description" content="ABI-PROJECT">
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                  <meta name="author" content="CNS-TEAM" >
+
+                  <title>Search</title>
+
+                  <link rel="stylesheet" href="css/main.css">
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+                  <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" charset="utf-8"></script>
+
+
+
+
+        </head>
+<body>
+     <nav class="navbar navbar-default navbar-fixed-top">
+     <div class="container">
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNav" aria-expanded="false">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="index.php"></a>
+       </div>
+       <div id="myNav" class="collapse navbar-collapse">
+         <ul class="nav navbar-nav">
+           <li><a href="index.php">Home</a></li>
+                      <li><a href="#">Contact</a></li>
+        </ul>
+                 <form id="signin" class="navbar-form navbar-right" role="form" action="login.php" method="post">
+          <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+              </div>
+     </div>
+   </nav>
+
+
+ 
+  <div class="container">
+    <div class="jumbotron">
+      <header>
+          <div class="row">
+    <div class="col-md-3">
+      <a href="#"><img src="#" border="0" class="img-responsive"/></a>
+    </div>
+    <div class="col-md-9">
+      <div class="row">
+        <h1>ABI</h1>
+        </div>
+        <div class="row">
+          <div class="lead">
+            Commercial Management
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+        </header>
+    </div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 text-center">
+        <h1>Bienvenue !</h1>
+              </div>
+    </div>
+  </div>
+
+</body>
+
+  </html>
