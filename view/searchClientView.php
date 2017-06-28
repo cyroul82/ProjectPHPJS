44feb5f1f478710a4dc2da1205db34ee19487e98
semsK <sems@live.fr>
2017-06-28 11:39:49 +0200

    working on search dropdown menu

--- view/searchClientView.php
+++ view/searchClientView.php
@@ -1,7 +1,6 @@
 <?php
   require("view/clientListTemplateView.php");
-  function displaySearch2($data){
-
+  function displaySearch(){
 ?>
 
 <!-- Barre de recherche -->
@@ -9,7 +8,7 @@
           <fieldset>
         <legend>Recherche de clients</legend>
 
-         <ul class="nav navbar-nav">
+<!--          <ul class="nav navbar-nav">
             <li class="dropdown">
                 <a data-toggle="dropdown" class="dropdown-toggle" id="myTabDrop1"  href="#"> Search by <b class="caret"></b></a>
                 <ul aria-labelledby="myTabDrop1" role="menu" class="dropdown-menu dropdown-menu-inverse">
@@ -20,7 +19,17 @@
                   <li><a data-toggle="tab" id="5" href="#dropdown5">Activity</a></li>         
                 </ul>
             </li>
-          </ul>
+          </ul> -->
+
+          <select name="select">
+            <option value="1">All</option>
+            <option value="2">ID Client</option>
+            <option value="3">Name</option>
+            <option value="4">City</option>
+            <option value="6">Activity</option>
+          </select>
+
+
 
           <form class="navbar-form navbar-right inline-form">
             <div class="form-group">
@@ -29,15 +38,7 @@
             </div>
           </form>
 
-                <div class="resultatTableau">
-                  
-
-
-
-
-                </div>
             </fieldset> 
-        </div>  
-<?php } ?>
-
-
+        </div>
+<?php
+    }    ?> 
