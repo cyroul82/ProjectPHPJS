d3816fec6997d34a140852e0fe77e2eb292370d5
semsK <sems@live.fr>
2017-06-28 12:11:36 +0200

    fixed bug listClient

--- view/modules/clientListView.php
+++ view/modules/clientListView.php
@@ -1,13 +1,13 @@
 
 <?php
 // This function display the list of Clients of ABI
-require("/clientListTemplateView.php");
+require("clientListTemplateView.php");
 function displayClientList($data){
 ?>
   <div class="container">
     <fieldset>
       <legend>Liste de tous les clients</legend>
-            <?php ClientTable($data);?>
+            <?php clientTable($data);?>
     </fieldset>
   </div>
   <?php } ?>
